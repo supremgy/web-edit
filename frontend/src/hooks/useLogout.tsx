8b60e278960f47c7c9b7f51421b6c0b4705715ab
supremgy <supremgy@icloud.com>
2024-03-30 20:02:09 +0900

    refactored useLogout login with react query

--- frontend/src/hooks/useLogout.tsx
+++ frontend/src/hooks/useLogout.tsx
@@ -1,16 +1,14 @@
 import { requestLogout } from '@/apis/user';
-import { useAuthStore } from '@/store/authStore';
-import { useNavigate } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useLogout = () => {
-  const { storeLogout } = useAuthStore();
-  const navigate = useNavigate();
-  const userLogout = () => {
-    requestLogout().then((res) => {
-      storeLogout();
-      alert(res.message);
-      navigate('/');
-    });
-  };
-  return { userLogout };
+  const queryClient = useQueryClient();
+
+  const logoutMutation = useMutation({
+    mutationFn: requestLogout,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['currnetUser'] });
+    },
+  });
+  return { logout: logoutMutation.mutateAsync };
 };
