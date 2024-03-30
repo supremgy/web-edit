ec5fe36485f1e81bdf1bc2deed7f0f173ae8f843
supremgy <supremgy@icloud.com>
2024-03-30 20:01:49 +0900

    refactored useJoin page

--- frontend/src/hooks/useJoin.tsx
+++ frontend/src/hooks/useJoin.tsx
@@ -1,10 +1,11 @@
 import { requestJoin } from '@/apis/user';
 import { LoginProps } from '@/components/LoginForm';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import to from 'await-to-js';
 import { isAxiosError } from 'axios';
 
 export const useJoin = () => {
+  const queryClient = useQueryClient();
   const joinMutation = useMutation({
     mutationFn: async (params: LoginProps) => {
       const [error] = await to(requestJoin(params));
@@ -19,6 +20,9 @@ export const useJoin = () => {
         throw error;
       }
       return { result: 'success' as const };
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['currentUser'] });
     },
   });
 
